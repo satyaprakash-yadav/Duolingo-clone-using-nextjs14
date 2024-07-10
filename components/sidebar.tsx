7a76487b8c08e34180712ae3e03d9e9dba9e603c
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-07-10 23:23:55 +0530

    feat: Sidebar Added

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import { cn } from "@/lib/utils";
+import { SidebarItem } from "@/components/sidebar-item";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type props = {
   className?: string;
@@ -8,11 +14,48 @@ export const Sidebar = ({ className }: props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
-        className,
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gay-y-2 flex-1">
+        <SidebarItem 
+          label="Learn" 
+          href="/learn" 
+          iconSrc="/learn.svg" 
+        />
+        <SidebarItem
+          label="leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem 
+          label="quests" 
+          href="/quests" 
+          iconSrc="/quests.svg" 
+        />
+        <SidebarItem 
+          label="shop" 
+          href="/shop" 
+          iconSrc="/shop.svg" 
+        />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
