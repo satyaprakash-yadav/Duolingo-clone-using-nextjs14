05a661692f97eb319f4fd0d06ede702b5ab383ba
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-07-12 23:06:17 +0530

    feat: Drizzle & Neon Database Added

---
+++ drizzle.config.ts
@@ -0,0 +1,21 @@
+import "dotenv/config"
+// import type { Config } from "drizzle-kit"
+import {defineConfig} from "drizzle-kit"
+
+// export default {
+//     schema: "./db/schema.ts",
+//     out: "./drizzle",
+//     driver: "postgresql",
+//     dbCredentials: {
+//         connectionString: process.env.DATABASE_URL!,
+//     },
+// } satisfies Config
+
+export default defineConfig({
+    schema: "./db/schema.ts",
+    out: "./drizzle",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    }
+})
