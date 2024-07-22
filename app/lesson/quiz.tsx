87ffcb40d808d5d9c4396dc90ce82d2872babfa0
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-07-23 00:03:50 +0530

    feat: Challenge Cards Added

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
-import { Header } from "./header";
+import { challengeOptions, challenges } from "@/db/schema";
+import { Header } from "@/app/lesson/header";
+import { QuestionBubble } from "@/app/lesson/question-bubble";
+import { Challenge } from "@/app/lesson/challenge";
 
 type Props = {
   initialPercentage: number;
@@ -24,6 +26,21 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(50 || initialPercentage);
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
 
   return (
     <>
@@ -32,6 +49,29 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div className="">
+              {/* TODO: Challenge Component & Change back to type ASSIST */}
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={() => {}}
+                status="none"
+                selectedOption={undefined}
+                disabled={false}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
